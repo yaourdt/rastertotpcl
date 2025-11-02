5063925ce2cabcf2948d7e4a17aacfa107122efa
Mark Dornbach <mark@dornbach.io>
2025-11-02 14:14:40 +0100

    Refactoring 6: Move IPP helpers to dedicated module

---
+++ src/tpcl-ipp-utils.c
@@ -0,0 +1,216 @@
+/*
+ * TPCL IPP Helpers Implementation
+ *
+ * IPP attribute handling helpers for Toshiba TEC label printers.
+ *
+ * Copyright © 2025 by Mark Dornbach
+ *
+ * Licensed under GNU GPL v3.
+ */
+
+#include "tpcl-ipp-utils.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/*
+ * 'tpcl_get_int_option()' - Get integer option from IPP attributes
+ */
+
+int
+tpcl_get_int_option(
+  ipp_t                    *attrs,
+  const char               *name,
+  int                      default_val,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+)
+{
+  char attr_name[256];
+  int value = default_val;
+
+  snprintf(attr_name, sizeof(attr_name), "%s-default", name);
+  ipp_attribute_t *attr = ippFindAttribute(attrs, attr_name, IPP_TAG_INTEGER);
+
+  if (attr)
+  {
+    value = ippGetInteger(attr, 0);
+    if (job)
+      papplLogJob(job, PAPPL_LOGLEVEL_DEBUG, "Retrieved %s from printer settings: %d", name, value);
+    else if (printer)
+      papplLogPrinter(printer, PAPPL_LOGLEVEL_DEBUG, "Retrieved %s from printer settings: %d", name, value);
+  }
+  else
+  {
+    if (job)
+      papplLogJob(job, PAPPL_LOGLEVEL_DEBUG, "Using default %s: %d", name, default_val);
+    else if (printer)
+      papplLogPrinter(printer, PAPPL_LOGLEVEL_DEBUG, "Using default %s: %d", name, default_val);
+  }
+
+  return value;
+}
+
+
+/*
+ * 'tpcl_get_str_option()' - Get string option from IPP attributes
+ */
+
+const char*
+tpcl_get_str_option(
+  ipp_t                    *attrs,
+  const char               *name,
+  const char               *default_val,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+)
+{
+  char attr_name[256];
+  const char *value = default_val;
+
+  snprintf(attr_name, sizeof(attr_name), "%s-default", name);
+  ipp_attribute_t *attr = ippFindAttribute(attrs, attr_name, IPP_TAG_KEYWORD);
+
+  if (attr)
+  {
+    value = ippGetString(attr, 0, NULL);
+    if (job)
+      papplLogJob(job, PAPPL_LOGLEVEL_DEBUG, "Retrieved %s from printer settings: %s", name, value);
+    else if (printer)
+      papplLogPrinter(printer, PAPPL_LOGLEVEL_DEBUG, "Retrieved %s from printer settings: %s", name, value);
+  }
+  else
+  {
+    if (job)
+      papplLogJob(job, PAPPL_LOGLEVEL_DEBUG, "Using default %s: %s", name, default_val);
+    else if (printer)
+      papplLogPrinter(printer, PAPPL_LOGLEVEL_DEBUG, "Using default %s: %s", name, default_val);
+  }
+
+  return value;
+}
+
+
+/*
+ * 'tpcl_add_vendor_int_option()' - Add integer vendor option
+ */
+
+void
+tpcl_add_vendor_int_option(
+  pappl_pr_driver_data_t   *driver_data,
+  ipp_t                    **driver_attrs,
+  const char               *name,
+  int                      min,
+  int                      max,
+  int                      default_val
+)
+{
+  char supported_name[256];
+  char default_name[256];
+
+  driver_data->vendor[driver_data->num_vendor] = name;
+
+  snprintf(supported_name, sizeof(supported_name), "%s-supported", name);
+  snprintf(default_name, sizeof(default_name), "%s-default", name);
+
+  ippAddRange(*driver_attrs, IPP_TAG_PRINTER, supported_name, min, max);
+  ippAddInteger(*driver_attrs, IPP_TAG_PRINTER, IPP_TAG_INTEGER, default_name, default_val);
+
+  driver_data->num_vendor++;
+}
+
+
+/*
+ * 'tpcl_add_vendor_str_option()' - Add string vendor option
+ */
+
+void
+tpcl_add_vendor_str_option(
+  pappl_pr_driver_data_t   *driver_data,
+  ipp_t                    **driver_attrs,
+  const char               *name,
+  int                      num_values,
+  const char               **values,
+  const char               *default_val
+)
+{
+  char supported_name[256];
+  char default_name[256];
+
+  driver_data->vendor[driver_data->num_vendor] = name;
+
+  snprintf(supported_name, sizeof(supported_name), "%s-supported", name);
+  snprintf(default_name, sizeof(default_name), "%s-default", name);
+
+  ippAddStrings(*driver_attrs, IPP_TAG_PRINTER, IPP_TAG_KEYWORD, supported_name, num_values, NULL, values);
+  ippAddString(*driver_attrs, IPP_TAG_PRINTER, IPP_TAG_KEYWORD, default_name, NULL, default_val);
+
+  driver_data->num_vendor++;
+}
+
+
+/*
+ * 'tpcl_get_label_dimensions()' - Calculate label dimensions from printer settings
+ */
+
+bool
+tpcl_get_label_dimensions(
+  ipp_t                    *printer_attrs,
+  int                      print_width,
+  int                      print_height,
+  int                      *label_pitch,
+  int                      *roll_width,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+)
+{
+  int label_gap = 50;  // Default: 5mm
+  int roll_margin = 10;  // Default: 1mm
+
+  // Get label gap from printer settings (0.1mm)
+  label_gap = tpcl_get_int_option(printer_attrs, "label-gap", 50, job, printer);
+
+  // Get roll margin from printer settings (0.1mm)
+  roll_margin = tpcl_get_int_option(printer_attrs, "roll-margin", 10, job, printer);
+
+  // Calculate label pitch and roll width from retrieved values
+  *label_pitch = print_height + label_gap;
+  *roll_width = print_width + roll_margin;
+
+  if (job)
+    papplLogJob(job, PAPPL_LOGLEVEL_DEBUG, "Calculated label dimensions from page size: width=%d (0.1mm), height=%d (0.1mm), pitch=%d (0.1mm), roll=%d (0.1mm)",
+                print_width, print_height, *label_pitch, *roll_width);
+  else if (printer)
+    papplLogPrinter(printer, PAPPL_LOGLEVEL_DEBUG, "Calculated label dimensions: width=%d (0.1mm), height=%d (0.1mm), pitch=%d (0.1mm), roll=%d (0.1mm)",
+                    print_width, print_height, *label_pitch, *roll_width);
+
+  return true;
+}
+
+
+/*
+ * 'tpcl_get_feed_adjustments()' - Get feed adjustment values from printer settings
+ */
+
+bool
+tpcl_get_feed_adjustments(
+  ipp_t                    *printer_attrs,
+  int                      *feed_adjustment,
+  int                      *cut_position_adjustment,
+  int                      *backfeed_adjustment,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+)
+{
+  // Get feed adjustment from printer settings (0.1mm)
+  *feed_adjustment = tpcl_get_int_option(printer_attrs, "feed-adjustment", 0, job, printer);
+
+  // Get cut position adjustment from printer settings (0.1mm)
+  *cut_position_adjustment = tpcl_get_int_option(printer_attrs, "cut-position-adjustment", 0, job, printer);
+
+  // Get backfeed adjustment from printer settings (0.1mm)
+  *backfeed_adjustment = tpcl_get_int_option(printer_attrs, "backfeed-adjustment", 0, job, printer);
+
+  // Return true if at least one adjustment is non-zero
+  return (*feed_adjustment != 0 || *cut_position_adjustment != 0 || *backfeed_adjustment != 0);
+}
---
+++ src/tpcl-ipp-utils.h
@@ -0,0 +1,177 @@
+/*
+ * TPCL IPP Helpers Header
+ *
+ * IPP attribute handling helpers for Toshiba TEC label printers.
+ *
+ * Copyright © 2025 by Mark Dornbach
+ *
+ * Licensed under GNU GPL v3.
+ */
+
+#ifndef TPCL_IPP_HELPERS_H
+#define TPCL_IPP_HELPERS_H
+
+#include <pappl/pappl.h>
+
+
+/*
+ * 'tpcl_get_int_option()' - Get integer option from IPP attributes
+ *
+ * Retrieves an integer option value from IPP attributes by looking for
+ * an attribute named "<name>-default".
+ *
+ * Parameters:
+ *   attrs        - IPP attributes (usually printer driver attributes)
+ *   name         - Base name of the option (without "-default" suffix)
+ *   default_val  - Default value to return if attribute not found
+ *   job          - Job for logging (can be NULL)
+ *   printer      - Printer for logging (can be NULL)
+ *
+ * Returns:
+ *   Integer value from attribute or default_val if not found
+ */
+
+int tpcl_get_int_option(
+  ipp_t                    *attrs,
+  const char               *name,
+  int                      default_val,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+);
+
+
+/*
+ * 'tpcl_get_str_option()' - Get string option from IPP attributes
+ *
+ * Retrieves a string/keyword option value from IPP attributes by looking
+ * for an attribute named "<name>-default".
+ *
+ * Parameters:
+ *   attrs        - IPP attributes (usually printer driver attributes)
+ *   name         - Base name of the option (without "-default" suffix)
+ *   default_val  - Default string to return if attribute not found
+ *   job          - Job for logging (can be NULL)
+ *   printer      - Printer for logging (can be NULL)
+ *
+ * Returns:
+ *   String value from attribute or default_val if not found
+ */
+
+const char* tpcl_get_str_option(
+  ipp_t                    *attrs,
+  const char               *name,
+  const char               *default_val,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+);
+
+
+/*
+ * 'tpcl_add_vendor_int_option()' - Add integer vendor option
+ *
+ * Registers an integer vendor option with IPP attributes, adding both
+ * the supported range and default value.
+ *
+ * Parameters:
+ *   driver_data  - Driver data structure to register vendor option
+ *   driver_attrs - IPP attributes to add option to
+ *   name         - Base name of the option
+ *   min          - Minimum value for the range
+ *   max          - Maximum value for the range
+ *   default_val  - Default value
+ */
+
+void tpcl_add_vendor_int_option(
+  pappl_pr_driver_data_t   *driver_data,
+  ipp_t                    **driver_attrs,
+  const char               *name,
+  int                      min,
+  int                      max,
+  int                      default_val
+);
+
+
+/*
+ * 'tpcl_add_vendor_str_option()' - Add string vendor option
+ *
+ * Registers a string/keyword vendor option with IPP attributes, adding
+ * both the supported values and default value.
+ *
+ * Parameters:
+ *   driver_data  - Driver data structure to register vendor option
+ *   driver_attrs - IPP attributes to add option to
+ *   name         - Base name of the option
+ *   num_values   - Number of supported values
+ *   values       - Array of supported value strings
+ *   default_val  - Default value string
+ */
+
+void tpcl_add_vendor_str_option(
+  pappl_pr_driver_data_t   *driver_data,
+  ipp_t                    **driver_attrs,
+  const char               *name,
+  int                      num_values,
+  const char               **values,
+  const char               *default_val
+);
+
+
+/*
+ * 'tpcl_get_label_dimensions()' - Calculate label dimensions from printer settings
+ *
+ * Retrieves label gap and roll margin from printer IPP attributes and
+ * calculates the label pitch and roll width based on print dimensions.
+ *
+ * Parameters:
+ *   printer_attrs - Printer IPP attributes
+ *   print_width   - Print width in 0.1mm
+ *   print_height  - Print height in 0.1mm
+ *   label_pitch   - Output: label pitch (height + gap) in 0.1mm
+ *   roll_width    - Output: roll width (width + margin) in 0.1mm
+ *   job           - Job for logging (can be NULL)
+ *   printer       - Printer for logging (can be NULL)
+ *
+ * Returns:
+ *   true on success, false on failure
+ */
+
+bool tpcl_get_label_dimensions(
+  ipp_t                    *printer_attrs,
+  int                      print_width,
+  int                      print_height,
+  int                      *label_pitch,
+  int                      *roll_width,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+);
+
+
+/*
+ * 'tpcl_get_feed_adjustments()' - Get feed adjustment values from printer settings
+ *
+ * Retrieves feed adjustment, cut position adjustment, and backfeed adjustment
+ * values from printer IPP attributes.
+ *
+ * Parameters:
+ *   printer_attrs            - Printer IPP attributes
+ *   feed_adjustment          - Output: feed adjustment in 0.1mm
+ *   cut_position_adjustment  - Output: cut position adjustment in 0.1mm
+ *   backfeed_adjustment      - Output: backfeed adjustment in 0.1mm
+ *   job                      - Job for logging (can be NULL)
+ *   printer                  - Printer for logging (can be NULL)
+ *
+ * Returns:
+ *   true if at least one adjustment is non-zero, false otherwise
+ */
+
+bool tpcl_get_feed_adjustments(
+  ipp_t                    *printer_attrs,
+  int                      *feed_adjustment,
+  int                      *cut_position_adjustment,
+  int                      *backfeed_adjustment,
+  pappl_job_t              *job,
+  pappl_printer_t          *printer
+);
+
+
+#endif // TPCL_IPP_HELPERS_H
